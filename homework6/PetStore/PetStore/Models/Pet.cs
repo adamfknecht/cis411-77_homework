867673e35d9372779bbb094e9840b516f1ece8a1
Adam Knecht <adamfknecht@gmail.com>
2014-03-19 00:07:35 -0400

    Added Pet Model and CRUD Controllers

---
+++ homework6/PetStore/PetStore/Models/Pet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace PetStore.Models
+{
+    public class Pet
+    {
+        public int petID { get; set; }
+        public string petName { get; set; }
+        public string petDescription { get; set; }
+        public DateTime petReceivedDate { get; set; }
+        public int petQuantity { get; set; }
+        public decimal petPrice { get; set; }
+        public byte petPicture { get; set; }
+
+    }
+
+    public class PetDBContext : DbContext
+    {
+        public DbSet<Pet> Pets { get; set; }
+        
+    }
+}
